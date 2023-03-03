9ab1533e9d2ec83622f3c7d42f20d300b76b59d2
Alona <122108963+yelloally@users.noreply.github.com>
2023-03-04 00:33:05 +0100

    Add files via upload

---
+++ interface_poli/EmptyInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace interface_poli
+{
+	public interface EmptyInterface
+	{
+        //interface represents a method: makeSound.
+        interface IAnimal
+        {
+            //method signature for makeSound()
+            void makeSound();
+        }
+    }
+}
+
---
+++ interface_poli/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace interface_poli
+{
+    //implements the interface
+    class Dog : IAnimal
+    {
+        private string breed;
+
+        public Dog(string breed)
+        {
+            this.breed = breed;
+        }
+
+        //method which returns the sound which made doGs
+        public void makeSound()
+        {
+            Console.WriteLine("The " + breed + " dog says woof!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What breed is your dog?");
+            string breed = Console.ReadLine();
+
+            IAnimal myAnimal = new Dog(breed);
+
+            myAnimal.makeSound();
+
+            Console.ReadKey();
+        }
+    }
+}
